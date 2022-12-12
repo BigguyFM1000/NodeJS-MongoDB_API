81444a8a74f1ae8ea4576640b34855a9c33af05f
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2022-12-12 13:11:40 +0200

    Added employee model

---
+++ Backend/Models/employee.models.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    email: {
+        type: String,
+        
+    },
+    address: {
+        type: String
+    },
+    phone: {
+        type: Number
+    }
+});
+const employee = new mongoose.model('Employee', schema);
+
+module.exports = employee;
